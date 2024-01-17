114219e8ca86b18b64f052f9aef370dffa039fc8
Thuong-ironman <thuongpham311@gmail.com>
2024-01-17 10:57:51 +0100

    add configuration file

---
+++ A3_conf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import random
+import numpy as np
+
+N = 200        # horizon size
+dt = 1e-2        # time step
+
+w_u = 1e-8    # weight for control input
+w_x = dt
+w_v = dt    # weight for velocity cost
+nq = 1                              # number of joint position
+nv = 1                              # number of joint velocity
+nu = 1                              # number of control
+
+DATA_FOLDER = 'data/'     # your data folder name
+DATA_FILE_NAME = 'warm_start' # your data file name
+save_warm_start = 0
+use_warm_start = 0
+if use_warm_start:
+    INITIAL_GUESS_FILE = DATA_FILE_NAME
+else:
+    INITIAL_GUESS_FILE = None
+
+lowerPositionLimit = -np.pi      # min joint position
+upperPositionLimit = np.pi      # max joint position
+upperVelocityLimit = 10             # min joint velocity
+lowerVelocityLimit = -10            # min joint velocity
+lowerControlBound    = -9.81    # lower bound joint torque
+upperControlBound    = 9.81       # upper bound joint torque
+
+x_des_final = np.array([0,0])        # final desired joint position and veloci
