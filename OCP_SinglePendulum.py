9569ed1d3aa386bc6817cb0753a529d7ddf18f85
Thuong-ironman <thuongpham311@gmail.com>
2024-01-19 10:36:03 +0100

    integrate terminal cost in final cost

--- OCP_SinglePendulum.py
+++ OCP_SinglePendulum.py
@@ -60,7 +60,7 @@ class OcpSinglePendulum:
         for i in range(N):
             self.opti.subject_to(x[i+1,0] == x[i,0] + self.dt * x[i,1])
             self.opti.subject_to(x[i+1,1] == x[i,1] + self.dt * (u[i] + self.g * ca.sin(x[i,0])))
-            #self.opti.subject_to( x[i+1]==x[i] + self.dt*u[i] )
+
         if(self.u_min is not None and self.u_max is not None):
             for i in range(N):
                 self.opti.subject_to( self.opti.bounded(self.u_min, u[i], self.u_max))
@@ -71,7 +71,7 @@ class OcpSinglePendulum:
             for i in range(N+1):
                 self.opti.subject_to( self.opti.bounded(self.v_min, x[i,1], self.v_max))
         self.opti.subject_to(x[0,:].T== x_init)
-        self.opti.subject_to(x[-1,:] == x[N-1,:]) #add hard constraints on the final state
+        #self.opti.subject_to(x[-1,:] == x[N-1,:]) #add hard constraints on the final state
         # s_opts = {"max_iter": 100}
         opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.sb': 'yes'}
         self.opti.solver("ipopt", opts) #, s_opts)
@@ -109,19 +109,59 @@ class OcpSinglePendulumWithNNCost(OcpSinglePendulum):
 
     def objective_cost(self,N,x,x_des,u):
         cost = 0
-        running_costs = [None,]*(N+1)
-        for i in range(N+1):
+        running_costs = [None,]*(N)
+        for i in range(N):
             """
             running cost function = 
             """
-            running_costs[i] = self.w_x * (x[i,0] - x_des[0])**2 + self.w_v * (x[i,1] - x_des[1])**2 
-            if(i<N):
+            running_costs[i] = self.w_x * (x[i,0] - x_des[0])**2 + 0*self.w_v * (x[i,1] - x_des[1])**2 
+            if(i<N-1):
                 running_costs[i] += self.w_u* u[i]*u[i]
             cost += running_costs[i] 
-        cost += self.w_v*(x[N,1])**2
+        cost += self.w_v*(x[N-1,1])**2
         #Terminal cost
-        # terminal_cost = self.get_value(x[-1,:].T)
-        # running_costs[-1] = terminal_cost
-        # cost += terminal_cost
+        terminal_cost = self.get_value(x[-1,:].T)
+        running_costs[-1] = terminal_cost
+        cost += terminal_cost
         return cost
     
+    def OCP_setup(self, x_init, N, x_des, X_guess=None, U_guess=None):
+        self.opti = ca.Opti()
+        self.x = self.opti.variable(N)
+        self.v = self.opti.variable(N)
+        self.X = ca.horzcat(self.x,self.v)
+        self.u = self.opti.variable(N)
+        x = self.X
+        u = self.u
+        if(X_guess is not None):
+            for i in range(N):
+                self.opti.set_initial(x[i,:], X_guess[i,:])
+        else:
+            for i in range(N):
+                self.opti.set_initial(x[i,:], x_init)
+        if(U_guess is not None):
+            for i in range(N):
+                self.opti.set_initial(u[i], U_guess[i,:][0])
+
+        self.cost = self.objective_cost(N,x,x_des,u)
+
+        self.opti.minimize(self.cost)
+
+        for i in range(1,N):
+            self.opti.subject_to(x[i,0] == x[i-1,0] + self.dt * x[i-1,1])
+            self.opti.subject_to(x[i,1] == x[i-1,1] + self.dt * (u[i-1] + self.g * ca.sin(x[i-1,0])))
+
+        if(self.u_min is not None and self.u_max is not None):
+            for i in range(N):
+                self.opti.subject_to( self.opti.bounded(self.u_min, u[i], self.u_max))
+        # if(self.x_min is not None and self.x_max is not None):
+        #     for i in range(N+1):
+        #         self.opti.subject_to( self.opti.bounded(self.x_min, x[i,0], self.x_max))
+        if(self.v_min is not None and self.v_max is not None):
+            for i in range(N):
+                self.opti.subject_to( self.opti.bounded(self.v_min, x[i,1], self.v_max))
+        self.opti.subject_to(x[0,:].T== x_init)
+        #self.opti.subject_to(x[-1,:] == x[N-1,:]) #add hard constraints on the final state
+        # s_opts = {"max_iter": 100}
+        opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.sb': 'yes'}
+        self.opti.solver("ipopt", opts) #, s_opts)
